2fb2ffa8c9e76b291b05c52dd8efd8e8af326cfa
Fatih Bayram <101430232+since1071@users.noreply.github.com>
2023-09-08 23:43:38 +0300

    Add project files.

---
+++ E_Commerce_DataAccess/Data/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_DataAccess.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ E_Commerce_Models/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Models
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Category Name is required")]
+        public string Name { get; set; }
+    }
+}
